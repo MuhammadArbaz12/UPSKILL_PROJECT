e20316e7c145a91a59d79b62b62ff53ad8fe260f
MuhammadArbaz12 <158058681+MuhammadArbaz12@users.noreply.github.com>
2025-01-28 21:02:32 +0500

    "hm"

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -13,17 +13,17 @@ const handleUploadImage = () =>{
 
 }
   return (
-    <div className='flex  '> 
-        <img src={user.imageUrl} alt='user_img' />
-      <textarea noOfLines={2}/>
+    <div className='flex w-[full] gap-5 m-auto justify-between mt-2 bg-slate-300 rounded-md items-center '> 
+        <img className='w-[10%] h-full p-2 rounded-full' src={user.imageUrl} alt='user_img' />
+      <textarea className='flex w-full ' noOfLines={2}/>
       
-      <div className="">
-      <IoImagesSharp  onClick={()=>{
+      <div className="flex w-[40%] gap-5 h-full  ">
+      <IoImagesSharp color='#31511E' size={25}  onClick={()=>{
         IMAGEFILEREF.current.click()
       }}  /> 
       <input hidden  type='file' accept='images/*' ref={IMAGEFILEREF} onChange={handleUploadImage} />
 
-      <button>Post</button>
+      <button className='font-serif text-green-950 font-bold bg-white w-[50%] rounded-md'>Post</button>
       </div>
     </div>
   )
