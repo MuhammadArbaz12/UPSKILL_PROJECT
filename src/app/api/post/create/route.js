a96d1a739ec5baacbbc168cdf29878a988b921de
MuhammadArbaz12 <158058681+MuhammadArbaz12@users.noreply.github.com>
2025-02-25 22:21:04 +0500

    "set""

--- src/app/api/post/create/route.js
+++ src/app/api/post/create/route.js
@@ -41,32 +41,31 @@ import Post from "../../../../lib/models/post.model";
 import { Connect } from "../../../../lib/mongodb/mongoose.js";
 import { currentUser } from "@clerk/nextjs/server";
 
-export async function POST(req) {
-    const user = await currentUser(req);
-    console.log("Received Data====>:", data);
-    try {
-        await Connect();
-        const data = await req.json();
-       
-        if (!user || user.publicMetadata.userMongoId !== data.userMongoId) {
-            return new Response("Unauthorized", { status: 504 });
-        }
-
-        const newPost = await Post.create({
-            user: data.userMongoId,
-            name: data.name,
-            username: data.username,
-            text: data.text,
-            profileImg: data.profileImg,
-            image: data.image,
-        });
-
-        await newPost.save();
-
-        return new Response(JSON.stringify(newPost), { status: 200 });
-    } catch (error) {
-        console.error("Error creating post:", error.message);
-        return new Response(error.message);
+export  async function POST(req) {
+        const user = await currentUser(req);
+        try {
+            await connect();
+            const data = await req.json();
+    console.log("Received Data:", data); 
+            if (!user || user.publicMetadata.userMongoId !== data.userMongoId) {
+                return new Response("Unauthorized", { status: 401 });
+            }
+    
+            const newPost = await Post.create({
+                user: data.userMongoId,
+                name: data.name,
+                username: data.username,
+                text: data.text,
+                profileImg: data.profileImg,
+                image: data.image,
+            });
+    
+            await newPost.save();
+    
+            return new Response(JSON.stringify(newPost), { status: 200 });
+        }catch (error) {
+        console.error("Error creating post:", error);
+        return new Response(JSON.stringify({ message: "Internal Server Error", error: error.message }), { status: 500 });
     }
-
-}
+    
+    }
